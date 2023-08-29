3868f9384ddd0f0dec5e05e85edba0d4f02ba4fd
Gid <gideonandrewedu@gmail.com>
2023-08-29 15:17:30 +0000

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,39 +1,14 @@
-#!/usr/bin/python3
-
 import requests
 
 def number_of_subscribers(subreddit):
-    # Set a custom User-Agent to avoid any issues with Reddit's API
-    headers = {'User-Agent': 'My Reddit API Client'}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Custom User Agent"}  # Add your custom User-Agent here
 
-    # Construct the URL to query the subreddit's information
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, headers=headers)
 
-    # Send a GET request to the Reddit API without following redirects
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the request was successful
     if response.status_code == 200:
-        try:
-            # Parse the JSON response
-            data = response.json()
-            # Extract the number of subscribers from the response
-            subscribers = data['data']['subscribers']
-            return subscribers
-        except KeyError:
-            # If the necessary key is not found in the response JSON
-            return 0
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
     else:
-        # If the subreddit is invalid or there's an issue with the request
         return 0
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        subscribers = number_of_subscribers(subreddit)
-        print("{:d}".format(subscribers))
-
