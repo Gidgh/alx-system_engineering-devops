39678ee77a83c7e272186fa1b275dcec72e3cca4
Gid <gideonandrewedu@gmail.com>
2023-08-29 16:35:37 +0000

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
+"""
+This is a module that uses the get HTTP header method to get resources
+from the Reddit API
+"""
+from requests import get
 
-import requests
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom User Agent"}  # Add your custom User-Agent here
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
-    else:
+    """
+    This is a function that gets the number of total subscribers for a given
+    subreddit using the REDDIT API
+    """
+    if subreddit is None or type(subreddit) is not str:
         return 0
+
+    user_agent = {'User-Agent': 'Google Chrome Version 115.0.0.0'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    resp = get(url, headers=user_agent, allow_redirects=False)
+    result = resp.json()
+    if resp.status_code == 404:
+        return 0
+    return result.get('data').get('subscribers')
